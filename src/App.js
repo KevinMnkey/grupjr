81234da91574bd106f5f55fdf18d86292c2ba6d1
KevinMnkey <kg739690@gmail.com>
2024-06-22 18:05:09 -0500

    prueba

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          estamos haciendo una pagina
         </a>
       </header>
     </div>
